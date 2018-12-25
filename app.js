8da7df18aa3df549b52948683e7451d2a8d28337
VictorKmin <kminvictor@gmail.com>
2018-12-25 17:28:55 +0200

    finish

--- app.js
+++ app.js
@@ -117,12 +117,46 @@
 
 // Cookies and session
 const express = require('express');
+
+
 const app = express();
+// Експрес сесія зберігає все на клієнті  і на серваку
+// const session = require('express-session');
+// app.use(session({
+//     // Ключ шифрування
+//     secret: 'super secret word',
+//     // Якщо сесія не мінялась, то вона не перезберігається
+//     resave: false,
+//     // Якщо не ініціалізована і не змінювалась. То нема чого пересейвлювати
+//     // Якщо створена але не мінялась
+//     saveUninitialized: false
+// }));
+
+
+// Кукі сесія зберігає на клієнті
+// Тут створеться 2 параметра в сесії.
+// Перший - це те, де зберігаються всі дані про нашу сесію
+// sig - це унікальна сигнатура сесії. Унікальний ідентифікатор
+// Кука - це шифровані за допомогою секретного слова данні.
+// Сигнатура - це наша шифрована кука, яка шифрується ключем
+const session = require('cookie-session');
+app.use(session({
+    secret: 'superSecret',
+    name: 'testCook',
+    keys: ['key']
+}));
+
+
+// Після того. як я заюзав сесію, я маю нові властивості в нашому реквесті
 
 app.get('/get', (req, res) => {
+    console.log(req.session);
     res.end('OK')
 });
 app.get('/set', (req, res) => {
+    req.session.password = '12345';
+    req.session.login = 'vaska';
+
     res.end('Set')
 });
 
